34e6c1f7236242c9c31582c1c8883a72449bc236
seejins <sejin@Sejins-MacBook-Pro.local>
2019-12-17 18:22:39 -0800

    initial cominitial commit, can search by genre and sort by title/rating

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+
+app.use(morgan('common'))
+app.use(cors())
+
+const playstore = require('./playstore.js')
+
+app.get('/playstore', (req, res) => {
+    const { genres, sort } = req.query
+
+    if (!sort && !genres) {
+        return res.send(playstore)
+    }
+
+    if (sort) {
+        if(!['Rating', 'App'].includes(sort)) {
+            return res.status(400).send('Sort must be one of Rating or App')
+        }
+    }
+
+    if (genres) {
+        if(!['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'].includes(genres)) {
+            return res.status(400).send('Genres must be one of Action, Puzzle, Strategy, Casual, Arcade, Card')
+        }
+    }
+
+    let results = playstore
+        .filter(apps =>
+            apps
+                .Genres
+                .toLowerCase()
+                .includes(genres.toLowerCase()))
+
+    if (sort) {
+        results.sort((a, b) => {
+            return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? - 1 : 0
+        })
+    }
+
+    res.json(results)
+})
+
+app.listen(8000, () => {
+    console.log('Server started on port 8000')
+})
